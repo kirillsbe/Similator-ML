bd2d89fb987e45f560dfa991b44d041ea53844b9
Kirill Yartsev <91340562+kirillsbe@users.noreply.github.com>
2023-06-13 14:47:07 +0500

    Add files via upload

---
+++ Intern/decisionTree1.sql
@@ -0,0 +1,5 @@
+select age, income, dependents, has_property, has_car, credit_score, job_tenure, has_education, loan_amount, 
+dateDiff('day', loan_start, loan_deadline) as loan_period, 
+if(dateDiff('day', loan_deadline, loan_payed) < 0, 0, dateDiff('day', loan_deadline, loan_payed))
+as delay_days from default.loan_delay_days
+order by id
